c4cb5534a879663b1fda586d15f5efcb186d3f3c
Mikel <soymikelnevado@gmail.com>
2026-03-20 13:03:06 +0100

    Initial commit

---
+++ src/main/java/com/dam1/lumaraesports/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.dam1.lumaraesports.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam1/lumaraesports/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam1.lumaraesports.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
